80532941f89f3cb4c72dd2711b7c0ad0db8977c0
Meta <Meta>
2024-04-01 22:01:13 +0200

    added multiple versions of the board (press keys 0-8) closes #14

--- src/components/views/Board.tsx
+++ src/components/views/Board.tsx
@@ -5,32 +5,56 @@ import {Button} from "components/ui/Button";
 import {useNavigate, useParams} from "react-router-dom";
 import "styles/views/Board.scss";
 
-const Board = () => {
 
+const Board = () => {
+    const [imageId, setImageId]=useState("0")
+
+    const KeyboardControls = () => {
+        const { zoomIn, zoomOut, resetTransform } = useControls();
+    
+        useEffect(() => {
+            const keyDownEvent = (event) => {
+                switch (event.key){
+                case "r": resetTransform();
+                default:
+                    if (event.key in ["1", "2", "3", "4", "5", "6", "7", "8", "0"])
+                        setImageId(event.key);
+                }
+            };
+    
+            window.addEventListener("keydown", keyDownEvent);
+    
+            return () => {
+                window.removeEventListener("keydown", keyDownEvent);
+            };
+        }, []);
+    
+        return null;
+    };
 
     useEffect(() => {
 
         document.body.classList.add("scrollbar-removal")
-        // window.addEventListener("keydown", keyPressEvent);
         
         return () => {
             document.body.classList.remove("scrollbar-removal")
-            // window.removeEventListener("keydown", keyPressEvent);
         };
     }, []);
 
+
     return (
         <div>
-            Top Ui
+            {/* Top UI doesn't work correctly, as it shrinks the main screen */}
             <div className="board-container">
                 Left UI
                 <TransformWrapper
                     disablePadding={true}
                     doubleClick={{mode:"toggle", step:1.3}}
                 >
+                    <KeyboardControls />
                     <TransformComponent>
                         <img
-                            src={ require("../../files/boards/board_0.png")}
+                            src={ require("../../files/boards/board_".concat(imageId, ".png"))}
                             className="board-background"
                         />
                     </TransformComponent>
