c7fe128b6c80149dd0c5d754537ea66297250e8e
ambroseberhard <ambros.eberhard@gmx.ch>
2024-04-21 08:02:46 +0200

    basic template for agora

---
+++ src/helpers/agoraManager.js
@@ -0,0 +1,60 @@
+import AgoraRTC from "agora-rtc-sdk-ng"
+import config from "./config.json";
+
+const APP_ID = "f4ddb18335fb41e5a06d3f86f686060b";
+const TOKEN = "84b6ef731de847b7b626bb26725e94a9";
+
+const agoraRTCManager = async (eventsCallback) => {
+    let agoraEngine = null;
+    const setupAgoraEngine = () => {
+        agoraEngine = AgoraRTC.createClient({ mode: "rtc", codec: "vp9" });
+    };
+
+    setupAgoraEngine();
+
+    const getAgoraEngine = () => {
+        return agoraEngine;
+    };
+
+    agoraEngine.on("user-published", async (user, mediaType) => {
+        // Subscribe to the remote user when the SDK triggers the "user-published" event.
+        await agoraEngine.subscribe(user, mediaType);
+        console.log("subscribe success");
+        eventsCallback("user-published", user, mediaType)
+    });
+
+    // Listen for the "user-unpublished" event.
+    agoraEngine.on("user-unpublished", (user) => {
+        console.log(user.uid + "has left the channel");
+    });
+
+    const join = async (channelName, channelParameters) => {
+        await agoraEngine.join(
+            APP_ID,
+            channelName,
+            TOKEN
+        )
+
+        channelParameters.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+        await getAgoraEngine.publish([
+            channelParameters.localAudioTrack
+        ]);
+    }
+
+    const leave = async (channelParameters) => {
+        // Destroy the local audio and video tracks.
+        channelParameters.localAudioTrack.stop();
+        channelParameters.localAudioTrack.close();
+        // Remove the containers you created for the local video and remote video.
+        await agoraEngine.leave();
+    };
+
+    return {
+        getAgoraEngine,
+        config,
+        join,
+        leave
+    };
+};
+
+export default agoraRTCManager();
---
+++ src/helpers/agoraUtils.js
@@ -0,0 +1,44 @@
+import agoraManager from "./agoraManager";
+
+let channelParameters = {
+    // A variable to hold a local audio track.
+    localAudioTrack: null,
+    // A variable to hold a remote audio track.
+    remoteAudioTrack: null,
+    // A variable to hold a remote video track.
+    remoteUid: null,
+};
+
+const handleVSDKEvents = (eventName, ...args) => {
+    switch (eventName) {
+    case "user-published":
+        // Get the RemoteAudioTrack object in the AgoraRTCRemoteUser object.
+        channelParameters.remoteAudioTrack = args[0].audioTrack;
+        // Play the remote audio track. No need to pass any DOM element.
+        channelParameters.remoteAudioTrack.play();
+
+    }
+};
+
+const { join, leave, getAgoraEngine, setUserRole , config} = await AgoraGetStarted(
+    handleVSDKEvents, product
+);
+
+// Get an instance of the Agora Engine from the manager
+const agoraEngine = await getAgoraEngine();
+
+const joinVoice = async function () {
+    // Join a channel.
+    await join("main", channelParameters);
+    console.log("publish success!");
+};
+// Listen to the Leave button click event.
+const leaveVoice = async function () {
+    // Leave the channel
+    await leave(channelParameters);
+    console.log("You left the channel");
+    // Refresh the page for reuse
+    window.location.reload();
+};
+
+export {joinVoice,leaveVoice};
