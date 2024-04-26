5985dfbd3fc9e1233ef15d33f9659c4f5d936457
Meta <Meta>
2024-04-26 17:40:02 +0200

    connected board to websockets using subscriptions (maybe works?)

--- src/components/views/Board.tsx
+++ src/components/views/Board.tsx
@@ -1,8 +1,7 @@
 import React, {useEffect, useState, useRef} from "react";
 import {TransformWrapper, TransformComponent, useControls} from "react-zoom-pan-pinch";
 import "styles/views/Board.scss";
-
-const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
+import { useWebsocket } from "./Websockets";
 
 //#region 
 const ScalableOverlay: React.FC<{
@@ -50,11 +49,28 @@ const ScalableOverlay: React.FC<{
     />
     );
 }
+
+const PlayerStatus: React.FC<{
+    playerMoney: string;
+    playerColour: string;
+    items: string;
+    userName: string;
+}> = ({ playerMoney, playerColour, items , userName}) => {
+  
+    return (
+        <div className="player-status-box">
+            <font color={playerColour}>{userName}</font><br/>
+            Money: {playerMoney}<br/>
+            Only Fans: {items}
+        </div>
+    );
+};
 //#endregion
 
 //are to be loaded from burger.json/be sent from the backend
 const coordinates = {"1":[0.17012893982808, 0.440532417346501], "2":[0.641833810888252, 0.670244740231859], "3":[0.327722063037249, 0.504508372692143], "4":[0.600286532951289, 0.477458136539287], "5":[0.900787965616046, 0.613997423787033], "6":[0.322349570200573, 0.844568484328038], "7":[0.403295128939828, 0.293258909403177], "8":[0.333810888252149, 0.127093173035638], "9":[0.827722063037249, 0.772434521253757], "10":[0.876074498567335, 0.702018033490768], "11":[0.469914040114613, 0.850579647917561], "12":[0.893624641833811, 0.522971232288536], "13":[0.846704871060172, 0.440103048518678], "14":[0.790472779369627, 0.375268355517389], "15":[0.723853868194842, 0.321597252039502], "16":[0.660458452722063, 0.180334907685702], "17":[0.599212034383954, 0.133533705452984], "18":[0.413323782234957, 0.126234435379991], "19":[0.53474212034384, 0.851438385573207], "20":[0.285100286532951, 0.187634177758695], "21":[0.212392550143266, 0.20781451266638], "22":[0.169054441260745, 0.279089738085015], "23":[0.170845272206304, 0.36152855302705], "24":[0.192335243553009, 0.797337913267497], "25":[0.170487106017192, 0.522971232288536], "26":[0.169770773638968, 0.605839416058394], "27":[0.242836676217765, 0.693430656934307], "28":[0.26432664756447, 0.62043795620438], "29":[0.398638968481375, 0.585659081150708], "30":[0.459169054441261, 0.612709317303564], "31":[0.434813753581662, 0.67582653499356], "32":[0.421919770773639, 0.782739373121511], "33":[0.709885386819484, 0.741949334478317], "34":[0.253939828080229, 0.848003434950623], "35":[0.573424068767908, 0.670244740231859], "36":[0.507879656160459, 0.677114641477029], "37":[0.744985673352435, 0.581365392872477], "38":[0.727793696275072, 0.500644053241735], "39":[0.726361031518625, 0.407471017604122], "40":[0.613538681948424, 0.285959639330185], "41":[0.549426934097421, 0.321167883211679], "42":[0.492836676217765, 0.367969085444397], "43":[0.456661891117478, 0.433233147273508], "44":[0.543696275071633, 0.507943323314727], "45":[0.38932664756447, 0.8475740661228], "46":[0.660458452722063, 0.431515671962216], "47":[0.76432664756447, 0.826534993559468], "48":[0.338825214899713, 0.418205238299699], "49":[0.352793696275072, 0.337054529841133], "50":[0.314469914040115, 0.259768140832975], "51":[0.484598853868195, 0.210390725633319], "52":[0.612464183381089, 0.84070416487763], "53":[0.0744985673352436, 0.512666380420782], "54":[0.819842406876791, 0.612279948475741], "55":[0.427292263610315, 0.728209531987978], "56":[0.443409742120344, 0.251610133104337], "57":[0.175143266475645, 0.709317303563761], "58":[0.686962750716332, 0.837269214255045], "59":[0.726570200573066, 0.673380420781451], "60":[0.691618911174785, 0.249033920137398], "61":[0.51432664756447, 0.137398024903392], "62":[0.325573065902579, 0.588235294117647], "63":[0.453080229226361, 0.519106912838128]}
-const allItems=new Set<string>(["TheBrotherAndCo", "MagicMushroom", "SuperMagicMushroom", "UltraMagicMushroom", "OnlyFansSub", "TreasureChest"])
+const allItems=new Set<string>(["MagicMushroom", "TwoMushrooms", "TheBrotherAndCo", "PeaceImOut", "Fusion", "IceCreamChest", "WhatsThis", "SuperMagicMushroom", "Stick", "ImOut", "TreasureChest", "MeowYou", "XboxController", "BadWifi", "UltraMagicMushroom", "BestTradeDeal", "ItemsAreBelongToMe", "Confusion", "GoldenSnitch", "OnlyFansSub", "ChickyNuggie"])
+const allCards=new Set<string>(["B14", "B26", "B35", "B135", "B246", "B123", "B456", "B07", "S0", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "G13", "G26", "G45", "G04", "G37", "G1256"])
 const overSpaces=new Set<string>(["Junction", "Gate", "SpecialItem", "Goal"])
 
 
@@ -115,28 +131,80 @@ const junctionDataExample2 ={
         "hasKey": false //True if the Player has "Brothers…" False otherwwise
     }
 }
+
+const moneyDataExample1 = {
+    "address": "/topic/board/money",
+    "data": {
+        "1": { //playerId
+            "newAmountOfMoney": 50, //new amount of money for player 1
+            "changeAmountOfMoney": 3 //amount of money
+        }
+        
+    }
+}
+
+const moneyDataExample2 = {
+    "address": "/topic/board/money",
+    "data": {
+        "1": { //playerId
+            "newAmountOfMoney": 0, //new amount of money for player 1
+            "changeAmountOfMoney": -3 //amount of money
+        },
+        "2": { //playerId
+            "newAmountOfMoney": 0, //new amount of money for player 1
+            "changeAmountOfMoney": -30 //amount of money
+        },
+        "3": { //playerId
+            "newAmountOfMoney": 0, //new amount of money for player 1
+            "changeAmountOfMoney": -300 //amount of money
+        },
+        "4": { //playerId
+            "newAmountOfMoney": 0, //new amount of money for player 1
+            "changeAmountOfMoney": -4 //amount of money
+        }
+    }
+}
+
+const turnDataExample={
+    "address": "/topic/board/newActivePlayer",
+    "data": {
+        "currentTurn": 10, //current turn number
+        "activePlayer": "1" //playerId
+    }
+}
+
+const goalDataExample={
+    "address": "/topic/board/goal",
+    "data": {
+        "result": 5
+    }
+}
 //#endregion
 
 const Board = () => { //NOSONAR
+    const {client , sendMessage, isConnected, disconnect} = useWebsocket();
     const hoverFilter="invert(54%) sepia(82%) saturate(1944%) hue-rotate(80deg) brightness(114%) contrast(126%)";
     const unlockedFilter="invert(14%) sepia(83%) saturate(7026%) hue-rotate(359deg) brightness(99%) contrast(109%)";
     const lockedFilter="invert(53%) sepia(8%) saturate(15%) hue-rotate(358deg) brightness(92%) contrast(92%)";
-
     const [imageId, setImageId]=useState("0"); //which goal state is used
     const [overlayActive, setOverlayActive]=useState(0);
     const [figurineSize, setFigurineSize]=useState("20px"); //actual size in pixels; starting value isn't seen under normal circumstances
     const [arrowSize, setArrowSize]=useState("20px"); //actual size in pixels; starting value isn't seen under normal circumstances
-    type ItemsState = {[key: string]: {[key: string]: number}};
+    type ItemsState = {[playerId: string]: {[itemName: string]: number}};
     const initialItems = Object.fromEntries(Array.from(allItems).map(item => [item, 0]));
     const [items, setItems]=useState<ItemsState>({"1": initialItems, "2": initialItems, "3": initialItems, "4": initialItems})
     const relativeFigurineSize=.025 //figurine width in % of boardwidth
     const relativeArrowSize=.035 //arrow width in % of boardwidth
-    const [playerSpace, setPlayerSpace]=useState({"1": 1, "2": 2, "3": 3, "4": 4});
-    const [playerMoney, setPlayerMoney]=useState({"1": 0, "2": 100, "3": 10, "4": 25});
+    const [playerSpace, setPlayerSpace]=useState({"1": 1, "2": 2, "3": 3, "4": 1});
+    const [playerMoney, setPlayerMoney]=useState({"1": 0, "2": 0, "3": 0, "4": 0});
+    const [turnNumber, setTurnNumber]=useState(0);
+    const [activePlayer, setActivePlayer]=useState("0");
     const [playerColour, setPlayerColour]=useState({"1":"yellow", "2":"green", "3":"blue", "4":"red"})
+    const [displayPlayerIds, setDisplayPlayerIds]=useState(["1", "2", "3", "4"]) //This Player, Teammate, Enemy, Enemy
+    const [userNames, setUserNames]=useState({"1": "Player 1", "2": "Player 2", "3": "Player 3", "4": "Player 4"})
     const [arrowPositions, setArrowPositions]=useState(null) //null if there are no arrows, otherwise [[from, to, locked?]]
     const boardRef=useRef(null);
-    const figurineGlobalOffset=[-1.21, -2] //offset to center figurines on the spaces
+    const figurineGlobalOffset=[-1.3, -2.05] //offset to center figurines on the spaces
     const arrowGlobalOffset=[1.9, 2.1] //offset to correct arrow positioning
     const multipleFigurinesDisplacement = {"1":[[0, 0]], "2":[[-1.3, 0], [1.3, 0]], "3": [[-1.8, .3], [1.8, .3], [0, -.55]], "4": [[0, 1.8], [1.8, 0], [-1.8, 0], [0, -1.8]]} //displacement in board width percentage when multiple players are on one space
 
@@ -170,12 +238,10 @@ const Board = () => { //NOSONAR
         case "tp":
         case "teleport":
             toRead["movementType"]="simultaneous";
-
-            return move(toRead);
+            move(toRead);
         case "jump":
             toRead["movementType"]="move";
-
-            return move(toRead);
+            move(toRead);
         default:
             throw new Error("notImplemented");
         }
@@ -199,7 +265,64 @@ const Board = () => { //NOSONAR
         return null
     }
 
+    const money = (data) => {
+        const updates = Object.entries(data).reduce((acc, [playerId, details]) => {
+            acc[playerId] = details["newAmountOfMoney"];
+
+            return acc;
+        }, {});
+      
+        setPlayerMoney(prevMoney => ({
+            ...prevMoney,
+            ...updates
+        }));
+    }
+
+    const goal = (data) => {
+        let res=data["result"]
+        setImageId(res)
+    }   
+    
+    const newActivePlayer = (data) => {
+        setTurnNumber(data["currentTurn"]);
+        setActivePlayer(data["activePlayer"])
+    }
+
+    //$ websockets
+
+    useEffect(() => {
+        if (client && isConnected){
+            client.subscribe("/topic/board/goal", (message) => {
+                const data = JSON.parse(message.body);
+                goal(data)
+            });
+
+            client.subscribe("/topic/board/junction", (message) => {
+                const data = JSON.parse(message.body);
+                junction(data)
+            });
+
+            client.subscribe("/topic/board/move", (message) => {
+                const data = JSON.parse(message.body);
+                move(data)
+            });
+
+            client.subscribe("/topic/board/money", (message) => {
+                const data = JSON.parse(message.body);
+                money(data)
+            });
+
+            client.subscribe("/topic/board/newActivePlayer", (message) => {
+                const data = JSON.parse(message.body);
+                money(data)
+            });
+        }
+
+    }, [client, isConnected, sendMessage, disconnect])
+
     //^ Helper functions
+
+    const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
 
     const addItem = (playerId: string, itemName: string) => {
         setItems(prevItems => ({
@@ -294,6 +417,21 @@ const Board = () => { //NOSONAR
                     setOverlayActive(0);
                     break;
                     //~ ↓ debug options, will be removed in the production build
+                case "n":
+                    setDisplayPlayerIds([displayPlayerIds[3], displayPlayerIds[0], displayPlayerIds[1], displayPlayerIds[2]])
+                    break;
+                case "m":
+                    money(moneyDataExample1["data"]);
+                    break;
+                case "p":
+                    newActivePlayer(turnDataExample["data"]);
+                    break;
+                case "g":
+                    goal(goalDataExample["data"]);
+                    break;
+                case "M":
+                    money(moneyDataExample2["data"]);
+                    break;
                 case "t":
                     (junction(junctionDataExample1["data"]));
                     break;
@@ -310,11 +448,9 @@ const Board = () => { //NOSONAR
                     (move(moveDataExample1["data"]));
                     break;
                 case "i":
-                    alert(items["1"]["OnlyFansSub"])
                     addItem("1", "OnlyFansSub")
                     break;
                 case "I":
-                    alert(items["1"]["OnlyFansSub"])
                     removeItem("1", "OnlyFansSub")
                     break;
                 case "q":
@@ -407,37 +543,32 @@ const Board = () => { //NOSONAR
                         alt="arrow"
                         pathToPicture={"figurines/arrow.svg"}
                         className="arrow-picture"
-                        clickFunction= {() => alert(id[1])}
+                        clickFunction= {() => sendMessage("/board/junction", JSON.stringify({"selectedSpace":id[1]}))}
                         colors={[id[2]===1 ? lockedFilter : unlockedFilter, hoverFilter]}
                     />))
                 : ""}
         </div>
     )
+
+    const playerElement = (playerId) => {
+        return (
+            <PlayerStatus   
+                userName={userNames[playerId]}
+                playerColour={playerColour[playerId]}
+                items={items[playerId]["OnlyFansSub"]}
+                playerMoney={playerMoney[playerId]}
+            />)
+    }
+
     
     return (
         <div>
             {/* Top UI doesn't work correctly, as it shrinks the main screen */}
             <div className="board-container">
                 <div className="player-status">
-                    <div className="player-status-box">
-                        <font color={playerColour["1"]}>Player: 1</font><br/>
-                        Money: {playerMoney["1"]}<br/>
-                        Only Fans: {items["1"]["OnlyFansSub"]}
-                    </div>
-                    <div className="player-status-box">
-                        <font color={playerColour["2"]}>Player: 2</font><br/>
-                        Money: {playerMoney["2"]}<br/>
-                        Only Fans: {items["2"]["OnlyFansSub"]}
-                    </div>
-                    <div className="player-status-box">
-                        <font color={playerColour["3"]}>Player: 3</font><br/>
-                        Money: {playerMoney["3"]}<br/>
-                        Only Fans: {items["3"]["OnlyFansSub"]}
-                    </div>
-                    {/* <div className="player-status-box">
-                        <font color={playerColour["4"]}>Player: 4</font><br/>
-                        Money: {playerMoney["4"]}
-                    </div> */}
+                    {playerElement(displayPlayerIds[2])}
+                    {playerElement(displayPlayerIds[3])}
+                    {playerElement(displayPlayerIds[1])}
                 </div>
                 <TransformWrapper
                     disablePadding={true}
@@ -462,7 +593,21 @@ const Board = () => { //NOSONAR
                         />
                     </TransformComponent>
                 </TransformWrapper>
-                Right UI
+                <div className="player-status">
+                    <div className="player-status-turn">
+                        {turnNumber}/20
+                    </div>
+                    {playerElement(displayPlayerIds[0])}
+                    <div className="player-status-controls">
+                        <button
+                            onClick={ () => sendMessage("/board/dice", JSON.stringify({}))}
+                            disabled={activePlayer===displayPlayerIds[0]}
+                        >
+                            Roll Dice
+                        </button><br/>
+                        {/* <button onClick={ () => alert("a")}>Use Item</button> */}
+                    </div>
+                </div>
             </div>
         </div>
 
