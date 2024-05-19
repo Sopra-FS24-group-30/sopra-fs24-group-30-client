b066803e101c1d6f59228b95beb62fee6f9b7ccc
celineziegler <you@example.com>
2024-05-19 13:58:29 +0200

    Changed MessageMapping

--- src/components/views/Board.tsx
+++ src/components/views/Board.tsx
@@ -601,7 +601,7 @@ const Board = () => { //NOSONAR
     //$ websockets
     useEffect(() => {
         if (client && isConnected){
-            const subscriptionStart = client.subscribe(`/user/queue/game/${gameId}/board/start`, (message)=>{
+            const subscriptionStart = client.subscribe(`/topic/game/${gameId}/board/start`, (message)=>{
                 const data = JSON.parse(message.body);
                 setTurnOrder(data.TurnOrder);
 
@@ -657,6 +657,8 @@ const Board = () => { //NOSONAR
                 gameEnd(data)
             });
 
+            sendMessage(`/app/game/${gameId}/board/start`, {userId});
+
             return () => {
                 subscriptionStart.unsubscribe();
                 subscrpitionGoal.unsubscribe();
@@ -669,8 +671,6 @@ const Board = () => { //NOSONAR
                 subscriptionGameEnd.unsubscribe();
             }
         }
-
-        sendMessage(`/app/game/${gameId}/board/start`, {userId});
 
     }, [client, isConnected, sendMessage, disconnect])
 
