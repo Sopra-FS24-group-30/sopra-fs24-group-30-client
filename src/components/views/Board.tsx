8f227e5428d282f3e3bbaec88fef540a9735f8b1
Meta <Meta>
2024-05-23 12:47:42 +0200

    added a queue and space land messages

--- src/components/views/Board.tsx
+++ src/components/views/Board.tsx
@@ -35,6 +35,8 @@ const {ceil, floor, min, max, round, abs} = Math; //NOSONAR this is way more con
 const colours={"yellow": "#fff155", "green": "#82ff55", "blue": "#55d9ff", "red": "#ff555d", "pink": "#ff8db2", "orange": "#ff8701", "white": "#ffffff", "purple": "#9500e5"}
 const cardColours={"Gold": ["#ffdd00", "#000"], "Silver": ["#898989", "#fff"], "Bronze": ["#e48518", "#fff"], "Ultimate": ["#b1001d", "#fff"], "WinCondition": ["#be8f3c", "#fff"]}
 
+const landOnMsg={"blue": " a blue Space.", "yellow": " a yellow Space.", "item": " an Item Space.", "card": " a Card Space.", "black": " a big oops Space.", "red": " a small oops Space.", "gambling": " a gambling Space.", "catnami": " a Catnami Space."}
+
 const lost="<font color=#de1313>lost</font>"
 const gained="<font color=#18c92a>gained</font>"
 
@@ -154,7 +156,7 @@ const PlayerStatus: React.FC<{
 //are to be loaded from burger.json/be sent from the backend
 const coordinates = {"1": [0.17012893982808, 0.440532417346501], "2": [0.641833810888252, 0.670244740231859], "3": [0.327722063037249, 0.504508372692143], "4": [0.600286532951289, 0.477458136539287], "5": [0.900787965616046, 0.613997423787033], "6": [0.322349570200573, 0.844568484328038], "7": [0.403295128939828, 0.293258909403177], "8": [0.333810888252149, 0.127093173035638], "9": [0.827722063037249, 0.772434521253757], "10": [0.876074498567335, 0.702018033490768], "11": [0.469914040114613, 0.850579647917561], "12": [0.893624641833811, 0.522971232288536], "13": [0.846704871060172, 0.440103048518678], "14": [0.790472779369627, 0.375268355517389], "15": [0.723853868194842, 0.321597252039502], "16": [0.660458452722063, 0.180334907685702], "17": [0.599212034383954, 0.133533705452984], "18": [0.413323782234957, 0.126234435379991], "19":[0.53474212034384, 0.851438385573207], "20":[0.285100286532951, 0.187634177758695], "21":[0.212392550143266, 0.20781451266638], "22":[0.169054441260745, 0.279089738085015], "23":[0.170845272206304, 0.36152855302705], "24":[0.192335243553009, 0.797337913267497], "25":[0.170487106017192, 0.522971232288536], "26":[0.169770773638968, 0.605839416058394], "27":[0.242836676217765, 0.693430656934307], "28":[0.26432664756447, 0.62043795620438], "29":[0.398638968481375, 0.585659081150708], "30":[0.459169054441261, 0.612709317303564], "31":[0.434813753581662, 0.67582653499356], "32":[0.421919770773639, 0.782739373121511], "33":[0.709885386819484, 0.741949334478317], "34":[0.253939828080229, 0.848003434950623], "35":[0.573424068767908, 0.670244740231859], "36":[0.507879656160459, 0.677114641477029], "37":[0.744985673352435, 0.581365392872477], "38":[0.727793696275072, 0.500644053241735], "39":[0.726361031518625, 0.407471017604122], "40":[0.613538681948424, 0.285959639330185], "41":[0.549426934097421, 0.321167883211679], "42":[0.492836676217765, 0.367969085444397], "43":[0.456661891117478, 0.433233147273508], "44":[0.543696275071633, 0.507943323314727], "45":[0.38932664756447, 0.8475740661228], "46":[0.660458452722063, 0.431515671962216], "47":[0.76432664756447, 0.826534993559468], "48":[0.338825214899713, 0.418205238299699], "49":[0.352793696275072, 0.337054529841133], "50":[0.314469914040115, 0.259768140832975], "51":[0.484598853868195, 0.210390725633319], "52":[0.612464183381089, 0.84070416487763], "53":[0.0744985673352436, 0.512666380420782], "54":[0.819842406876791, 0.612279948475741], "55":[0.427292263610315, 0.728209531987978], "56":[0.443409742120344, 0.251610133104337], "57":[0.175143266475645, 0.709317303563761], "58":[0.686962750716332, 0.837269214255045], "59":[0.726570200573066, 0.673380420781451], "60":[0.691618911174785, 0.249033920137398], "61":[0.51432664756447, 0.137398024903392], "62":[0.325573065902579, 0.588235294117647], "63":[0.453080229226361, 0.519106912838128]}
 const allUsables=new Set<string>(["MagicMushroom", "TwoMushrooms", "TheBrotherAndCo", "PeaceImOut", "IceCreamChest", "WhatsThis", "SuperMagicMushroom", "Stick", "ImOut", "TreasureChest", "MeowYou", "XboxController", "BadWifi", "UltraMagicMushroom", "BestTradeDeal", "ItemsAreBelongToMe", "Confusion", "GoldenSnitch", "OnlyFansSub", "ChickyNuggie", "B14", "B26", "B35", "B135", "B246", "B123", "B456", "B07", "S0", "S1", "S2", "S3", "S4", "S5", "S6", "S7", "G13", "G26", "G45", "G04", "G37", "G1256"])
-const overSpaces=new Set<string>(["junction", "gate", "specialitem", "goal"])
+const overSpaces=new Set<string>(["junction", "gate", "specialitem", "goal", "bluegoal"])
 
 //! only used for development purposes, to be removed in production build
 //#region 
@@ -436,7 +438,9 @@ const Board = () => { //NOSONAR
                                 }));
                                 await sleep(300);
                             }
-                            try{if (!(overSpaces.has(val["spaceColour"].toLowerCase()))) await timerMsg("Landed on Space", `${userNames[playerId]} landed on a ${val["spaceColour"]} Space.`, 1250)}
+                            // alert(val["SpaceColour"])
+                            // console.log(val)
+                            try{if (!(overSpaces.has(val["spaceColor"].toLowerCase()))) await timerMsg("Landed on Space", `${userNames[playerId]} landed on ${landOnMsg[val["spaceColor"].toLowerCase()]}.`, 1250)}
                             catch{}
                         }
                         resolve(null);
@@ -525,7 +529,7 @@ const Board = () => { //NOSONAR
                 await timerMsg("Change in Money", message);
                 resolve(null);
             } catch (error) {
-                reject(error);
+                resolve(null);
             }
         } else {
             resolve(null);
@@ -635,6 +639,58 @@ const Board = () => { //NOSONAR
         }
     })}
 
+    type CommandFunction = (arg: any) => Promise<unknown>|void;
+
+    interface queueFunction {
+        [key: string]: CommandFunction;
+    }
+    
+    class CommandProcessor {
+        private commandQueue: Array<{ name: string; data: any }> = [];
+        private processing: boolean = false;
+        private exampleFunctions: queueFunction;
+    
+        constructor(exampleFunctions: queueFunction) {
+            this.exampleFunctions = exampleFunctions;
+        }
+    
+        // Function to add commands to the queue using commandName and data directly
+        addToQueue(commandName: string, data: any) {
+            this.commandQueue.push({ name: commandName, data });
+            this.processQueue(); // Start processing the queue if not already doing so
+        }
+    
+        // Main loop to process commands from the queue
+        private async processQueue() {
+            if (this.processing) return;
+            this.processing = true;
+    
+            while (this.commandQueue.length > 0) {
+                const { name, data } = this.commandQueue.shift()!; // Take the first command
+                const func = this.exampleFunctions[name];
+    
+                if (typeof func === "function") {
+                    await func(data);
+                }
+            }
+    
+            this.processing = false;
+        }
+    }
+
+    const functionsForQueue: queueFunction = {
+        move,
+        dice,
+        junction,
+        goal,
+        newActivePlayer,
+        money,
+        usables,
+        ultimate,
+        winCondition,
+    };
+
+    
     //^ response to Websockets
 
     const sendArrowChoice = (choice) => {
@@ -667,30 +723,6 @@ const Board = () => { //NOSONAR
         sendMessage(address, {"used": usable, "choice": {}})
     }
 
-    async function processCommands(datata) {
-        const exampleFunctions: { [key: string]: (arg: any) => void } = {
-            move, //NOSONAR
-            junction,
-            goal,
-            newActivePlayer,
-            money,
-            sleep //NOSONAR
-        };
-
-        const commands = JSON.parse(datata);
-
-        for (const commandObject of commands) {
-
-            const commandName = Object.keys(commandObject)[0];
-            const commandData = commandObject[commandName];
-            const func = exampleFunctions[commandName];
-
-            if (typeof func === "function") {
-                await func(commandData); //NOSONAR
-            }
-
-        }
-    }
     //#endregion
 
     const sendMessageWeb = () => {
@@ -702,6 +734,7 @@ const Board = () => { //NOSONAR
     //$ websockets
     useEffect(() => {
         if (client && isConnected){
+            const processor = new CommandProcessor(functionsForQueue);
             const subscriptionStart = client.subscribe(`/topic/game/${gameId}/board/start`, (message)=>{
                 const data = JSON.parse(message.body);
                 console.log("Start PlayerInfo", data);
@@ -721,7 +754,9 @@ const Board = () => { //NOSONAR
 
             const subscrpitionGoal = client.subscribe(`/topic/game/${gameId}/board/goal`, (message) => {
                 const data = JSON.parse(message.body);
-                goal(data);
+                // goal(data);
+                processor.addToQueue("goal", data)
+                
             });
 
             const subscriptionError = client.subscribe(`/user/queue/game/${gameId}/board/error`, (message) => {
@@ -730,43 +765,56 @@ const Board = () => { //NOSONAR
 
             const subscriptionJunction = client.subscribe(`/user/queue/game/${gameId}/board/junction`, (message) => {
                 const data = JSON.parse(message.body);
-                junction(data)
+                // junction(data)
+                processor.addToQueue("junction", data)
+
             });
 
             const subscriptionUsables = client.subscribe(`/topic/game/${gameId}/board/usables`, (message) => {
                 const data = JSON.parse(message.body);
-                usables(data)
+                // usables(data)
+                processor.addToQueue("usables", data)
+
             });
 
             const subscriptionMove = client.subscribe(`/topic/game/${gameId}/board/move`, (message) => {
                 const data = JSON.parse(message.body);
                 console.log(data)
-                move(data)
+                // move(data)
+                processor.addToQueue("move", data)
+
             });
 
             const subscriptionMoney = client.subscribe(`/topic/game/${gameId}/board/money`, (message) => {
                 const data = JSON.parse(message.body);
-                money(data)
+                // money(data)
+                processor.addToQueue("money", data)
+
             });
 
             const subscriptionActivePlayer = client.subscribe(`/topic/game/${gameId}/board/newActivePlayer`, (message) => {
                 const data = JSON.parse(message.body);
-                newActivePlayer(data)
+                // newActivePlayer(data)
+                processor.addToQueue("newActivePlayer", data)
+
             });
 
             const subscriptionDice = client.subscribe(`/topic/game/${gameId}/board/dice`, (message) => {
                 const data = JSON.parse(message.body);
-                dice(data)
+                // dice(data)
+                processor.addToQueue("dice", data)
             });
 
             const subscriptionWinCondition = client.subscribe(`/user/queue/game/${gameId}/board/winCondition`, (message) => {
                 const data = JSON.parse(message.body);
-                winCondition(data.results)
+                // winCondition(data.results)
+                processor.addToQueue("winCondition", data)
             });
 
             const subscriptionUltimate = client.subscribe(`/user/queue/game/${gameId}/board/ultimative`, (message) => {
                 const data = JSON.parse(message.body);
-                ultimate(data.results)
+                // ultimate(data.results)
+                processor.addToQueue("ultimate", data)
             });
 
             const subscriptionGameEnd = client.subscribe(`/topic/game/${gameId}/board/gameEnd`, (message) => {
